4260826a2e0ae54b2292e7724463a05156b874af
king745379855 <745379855@qq.com>
2019-11-23 13:41:44 +0800

    Presenter去除多余代码

--- app/src/main/java/com/wzy/arm/di/presenter/HomePresenter.kt
+++ app/src/main/java/com/wzy/arm/di/presenter/HomePresenter.kt
@@ -4,20 +4,15 @@ import com.wzy.arm.di.contract.HomeContract
 import com.wzy.arms.base.BaseApp
 import com.wzy.arms.di.presenter.RxPresenter
 import com.wzy.arms.di.subscriber.BaseSubscriber
-import com.wzy.arms.network.helper.RetrofitHelper
 import com.wzy.arms.network.model.User
 import com.wzy.arms.utils.rxSchedulerFlowableHelper
-import javax.inject.Inject
 
 /**
  * @author: wzy
  * date: 2019/11/11
  * desc: 精选界面-SelectionPresenter
  */
-class HomePresenter
-@Inject
-constructor(private val mRetrofitHelper: RetrofitHelper) :
-        RxPresenter<HomeContract.View>(), HomeContract.Presenter<HomeContract.View> {
+class HomePresenter:RxPresenter<HomeContract.View>(), HomeContract.Presenter<HomeContract.View> {
     override fun requestUsers(since: Int?, perPage: Int?, update: Boolean?) {
         val subscriber = BaseApp.instance.getRepository()
                 .requestUsers(since, perPage, update!!)
